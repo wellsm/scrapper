dccaab6165d6d68d85f6a2c7c4f1ea4ca80b5b29
Well <wellington.martins@uello.com.br>
2025-01-03 16:28:34 -0300

    feat: logging scrapper url and selector

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import { format } from '@formkit/tempo'
 import puppeteer from "puppeteer";
 
 const app = express();
 const PORT = 3000;
+const TZ = 'America/Sao_Paulo';
 
 type Query = {
   [key: string]: string;
@@ -14,6 +16,10 @@ app.get("/scrape", async ({ query }: Request, res: Response): Promise<any> => {
   if (!url || !selector) {
     return res.status(400).json({ error: "URL and selector are required" });
   }
+
+  const date = format({ date: new Date(), format: 'YYYY-MM-DD HH:mm:ss', tz: TZ });
+
+  console.log(`${date} - ${url} - [${selector}]`);
 
   try {
     const browser = await puppeteer.launch({
